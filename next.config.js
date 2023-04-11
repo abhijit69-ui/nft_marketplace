fbb62d91ff6f86199509b6e4ea9971d82d169af8
abhijit <abhijitnath1a1@gmail.com>
2023-04-11 19:24:38 +0530

    added smartcontract

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  images: {
+    domains: ['ipfs.infura.io'],
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,25 @@
 import Script from 'next/script';
 import { ThemeProvider } from 'next-themes';
 
+import { NFTProvider } from '../context/NFTContext';
 import { Navbar, Footer } from '../components';
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
-  <ThemeProvider attribute="class">
-    <div className="dark:bg-nft-dark bg-white min-h-screen">
-      <Navbar />
-      <div className="pt-65">
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
 
-        <Component {...pageProps} />
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
 
-    <Script src="https://kit.fontawesome.com/03c4c981fc.js" crossOrigin="anonymous" />
-  </ThemeProvider>
+      <Script src="https://kit.fontawesome.com/03c4c981fc.js" crossOrigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
 );
 
 export default MyApp;
